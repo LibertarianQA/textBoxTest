cc8a18a037c974ac81aab944e87afafbddf47d16
Mukola Zhuravskii <rpglife85@gmail.com>
2024-03-12 10:02:55 +0200

    pressed f5 and saved

---
+++ src/test/java/demoqa/PracticeFormJavaFacker.java
@@ -0,0 +1,58 @@
+package demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormJavaFacker {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.holdBrowserOpen = true;
+
+    }
+@Test
+void fillPracticeForm(){
+        String firstName = "Mykola";
+        String lastName = "McKlay";
+        String userEmail = "123@gmail.com";
+        String userPhone = "0123456789";
+        String currentAddress = "na derevnu dedushke";
+
+
+        open("/automation-practice-form");
+        $(".text-center").shouldHave(text("Practice Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail );
+        $(".custom-control-label").click();
+        $("#userNumber").setValue(userPhone);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__day--015").click();
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#currentAddress").setValue(currentAddress);
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName),text(userEmail ) );
+
+
+}
+}
