a6f15d2ffbf59068cdd4e6485a721b6403db9a49
Mukola Zhuravskii <rpglife85@gmail.com>
2024-03-18 20:08:55 +0200

    @added AfterEach void addAttachments()

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -3,7 +3,9 @@ package demoqa;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import demoqa.helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -52,6 +54,12 @@ public class TestBase {
 @BeforeEach
     void addListener(){
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+}
+g{
+    Attach.screenshotAs("Last screenshot");
+    Attach.pageSource();
+    Attach.browserConsoleLogs();
+    Attach.addVideo();
 }
 
 }
