851d2b8fa7fcefd6588e6d5b3c5c5f9b8465bbf0
Mukola Zhuravskii <rpglife85@gmail.com>
2024-03-19 21:12:25 +0200

    + def remoute url

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -20,7 +20,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = System.getProperty("BROWSER_SIZE","1920x1080");
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote =System.getProperty("REMOTE_URL");
+        Configuration.remote =System.getProperty("REMOTE_URL","https://user1:1234@selenoid.autotests.cloud/wd/hub");
       //  Configuration.holdBrowserOpen = true;
         DesiredCapabilities options = new DesiredCapabilities();
         options.setCapability("browserVersion", "122.0");
